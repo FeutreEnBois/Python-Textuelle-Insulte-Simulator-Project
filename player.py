76dc8e89c0676d007591613e72251fec181ed902
FeutreEnBois <derosier.maxime@gmail.com>
2021-04-10 13:44:56 +0200

    classe player par Greg

---
+++ player.py
@@ -0,0 +1,16 @@
+class player:
+    def __init__(self,name):
+        self.name = name
+        if self.name == "La vieille":
+            self.pv = 60
+            self.resistance = "Famille"
+            self.weakness= "Animaux"
+        elif self.name == "L'étudiant":
+            self.pv = 50
+            self.resistance = "Travail"
+            self.weakness = "Argent"
+        elif self.name == "Mage":
+            self.pv = 40
+            self.resistance = "Lieux"
+            self.weakness = "Famille"
+        
