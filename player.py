03e89e912be65b8abf6e08f5b179660cfc652828
FeutreEnBois <derosier.maxime@gmail.com>
2021-04-10 16:59:00 +0200

    .\player.py by gregoire

--- player.py
+++ player.py
@@ -1,17 +1,37 @@
+# class player:
+#     def __init__(self,name,P):
+#         self.classe = ["La vieille", "L'étudiant", "Mage"]
+#         self.name = name
+#         self.P = P
+#         if self.name == "La vieille":
+#             self.pv = 60
+#             self.resistance = "Famille"
+#             self.weakness= "Animaux"
+#         elif self.name == "L'étudiant":
+#             self.pv = 50
+#             self.resistance = "Travail"
+#             self.weakness = "Argent"
+#         elif self.name == "Mage":
+#             self.pv = 40
+#             self.resistance = "Lieux"
+#             self.weakness = "Famille"
 class player:
-    def __init__(self,name):
-        self.classe = ["La vieille", 'Famille', "Travail", "Mage"]
+    def __init__(self, P ,name = "échec"):
         self.name = name
-        if self.name == "La vieille":
+        self.P = P
+        if name == 1:
+            self.name = "Mage"
+            self.pv = 60
+            self.resistance = "Lieux"
+            self.weakness = "Famille"
+        elif name == 2:
+            self.name = "La vieille"
             self.pv = 60
             self.resistance = "Famille"
             self.weakness= "Animaux"
-        elif self.name == "L'étudiant":
-            self.pv = 50
-            self.resistance = "Travail"
+        elif name == 3:
+            self.name = "L'étudiant"
+            self.pv = 60
+            self.resistance = ""
             self.weakness = "Argent"
-        elif self.name == "Mage":
-            self.pv = 40
-            self.resistance = "Lieux"
-            self.weakness = "Famille"
         
